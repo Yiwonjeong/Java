2f6d0e39ff7a903d034937425ec5d03eccd9cc4f
Yiwonjeong <near_and_dear_studio@naver.com>
2022-09-05 15:45:12 +0900

    2022/09/-5

---
+++ Ch07/src/sub1/Animal.java
@@ -0,0 +1,15 @@
+package sub1;
+
+public class Animal {
+
+		
+		public void move() {
+			System.out.println("Animal move...");
+		}
+		public void hunt() {
+			//animal의 기능은 필요없으니 그냥 선언만
+			//override를 위한 추상 method
+			
+		}
+
+}
